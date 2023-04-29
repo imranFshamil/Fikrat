20eb4c609da07f8759a5b070d7c7527c8e789631
fikrat99 <128935402+fikrat99@users.noreply.github.com>
2023-04-30 01:38:23 +0300

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,56 +1,61 @@
 <?php
-if(isset($_POST['email'])) {
- 
-    $email_to = "imranshamil75@gmail.com";
-    $email_subject = "Contact Form Submission";
- 
-    function died($error) {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
-    $name = $_POST['name']; 
-    $email_from = $_POST['email']; 
-    $message = $_POST['message']; 
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/';
- 
-    if(!preg_match($email_exp,$email_from)) {
-        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-    }
- 
-    if(strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br />';
-    }
- 
-    if(strlen($error_message) > 0) {
-        died($error_message);
-    }
- 
-    $email_message = "Form details below.\n\n";
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-Thank you for contacting us. We will be in touch with you very soon.
 
-<?php
+// Check if the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Set recipient email address
+    $to = 'imranshamil75@gmail.com';
+
+    // Set email subject
+    $subject = 'Contact Form Submission';
+
+    // Set email message body
+    $body = 'Name: ' . $_POST['name'] . "\n\n";
+    $body .= 'Email: ' . $_POST['email'] . "\n\n";
+    $body .= 'Message: ' . $_POST['message'] . "\n\n";
+
+    // Set email headers
+    $headers = 'From: ' . $_POST['email'] . "\r\n" .
+        'Reply-To: ' . $_POST['email'] . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        $success = true;
+    } else {
+        $error = 'Unable to send email. Please try again later.';
+    }
+
 }
+
 ?>
+
+<?php if (isset($success)): ?>
+
+    <p class="alert alert-success">Thank you for contacting us. We will be in touch with you very soon.</p>
+
+<?php else: ?>
+
+    <?php if (isset($error)): ?>
+
+        <p class="alert alert-danger"><?= $error ?></p>
+
+    <?php endif ?>
+
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+<?php endif ?>
