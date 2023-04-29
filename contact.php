e25aef0f901177dc85ed957af3dc3d3f2242186a
fikrat99 <128935402+fikrat99@users.noreply.github.com>
2023-04-29 23:36:24 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,22 +1,15 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    
-    // Set up the email headers and body
-    $to = 'youremail@example.com';
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-    $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
-    
-    // Send the email using the mail() function
-    if (mail($to, $subject, $body, $headers)) {
-        echo 'Message sent successfully!';
-    } else {
-        echo 'An error occurred. Please try again later.';
-    }
+if(isset($_POST['submit'])) {
+    $to = "your-email@example.com"; // Change this to your own email address
+    $subject = "Message from website form";
+    $message = "Full name: " . $_POST['fullname'] . "\n\n";
+    $message .= "Email address: " . $_POST['email'] . "\n\n";
+    $message .= "Telephone: " . $_POST['telephone'] . "\n\n";
+    $message .= "Service: " . $_POST['service'] . "\n\n";
+    $message .= "Message: " . $_POST['message'] . "\n\n";
+    $headers = "From: " . $_POST['email'] . "\r\n" .
+               "Reply-To: " . $_POST['email'] . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+    mail($to, $subject, $message, $headers);
 }
 ?>
